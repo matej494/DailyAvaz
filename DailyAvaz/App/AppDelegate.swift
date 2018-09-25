28e162689b693aa62e538fb7ed43a9a46619fd20
Matej Korman <matejkorman@matejs-mbp>
2018-09-25 13:09:33 +0200

    Project setup.

---
+++ DailyAvaz/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DailyAvaz
+//
+//  Created by Matej Korman on 25/09/2018.
+//  Copyright © 2018 Matej Korman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        return true
+    }
+}
