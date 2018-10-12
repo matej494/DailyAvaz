6e8c731ba0cc8eec86a0213ad04a164261260aac
Matej Korman <matejkorman@matejs-mbp>
2018-10-12 10:24:31 +0200

    Added `SingleImageTableViewCell` and installed pods for `Unit Testing`.

---
+++ DailyAvaz/App/Single/View/SingleImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageTableViewCell.swift
+//  DailyAvaz
+//
+//  Created by Matej Korman on 09/10/2018.
+//  Copyright © 2018 Matej Korman. All rights reserved.
+//
+
+import UIKit
+
+class SingleImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
